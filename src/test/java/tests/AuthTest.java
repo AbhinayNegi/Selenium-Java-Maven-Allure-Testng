d8c8b4965b8876ed5373480f639f8d182303e920
Laxmi Panwar <laxpanwar11@gmail.com>
2025-11-16 11:09:05 +0530

    Updated auth test.

--- src/test/java/tests/AuthTest.java
+++ src/test/java/tests/AuthTest.java
@@ -5,7 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
+ 
 import java.io.IOException;
 
 public class AuthTest extends BaseTest{
